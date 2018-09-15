98d4778509f3996dbb0ded9b7cdc346c9337defe
Anthony Junior Gonzalez Hernandez <anthony_gonzalez1@ucne.edu.do>
2018-09-15 14:11:13 -0400

    Ejercicios

---
+++ Ejercicios del 9-12/Cap 10/EjerciciosCap10.cs
@@ -0,0 +1,17 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Ejercicios_del_9_12.Cap_10
+{
+    class EjerciciosCap10
+    {
+        public void Ejercicios()
+        {
+            Console.WriteLine("Aun no Realizado");
+            Console.ReadKey();
+        }
+    }
+}
---
+++ Ejercicios del 9-12/Cap 12/EjerciciosCap12.cs
@@ -0,0 +1,17 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Ejercicios_del_9_12.Cap_12
+{
+    class EjerciciosCap12
+    {
+        public void Ejercicios()
+        {
+            Console.WriteLine("Aun no Realizado");
+            Console.ReadKey();
+        }
+    }
+}
